9d2ecae3e6616f9327ff622b744740d70cae3241
AGALMAD <agalmad@g.educaand.es>
2025-01-04 12:54:18 +0100

    cambiando el header y el main

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -10,45 +11,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  @HostListener('window:scroll', [])
-  checkScroll()
-  {
-    const currentScroll = window.scrollY;
-    const truck = document.getElementById("centered-image"); // Con esto se gestiona como de llena está la furgoneta
-    if(currentScroll < 300){
-      truck?.classList.remove("centered-image-2");
-      truck?.classList.add("centered-image");
-    }
-    else if(currentScroll >= 300 && currentScroll < 800){
-      truck?.classList.remove("centered-image");
-      truck?.classList.add("centered-image-2");
-      truck?.classList.remove("centered-image-3");
-    }
-    else if(currentScroll >= 800 && currentScroll <= 1500)
-    {
-      document.getElementById("second-animated-section")?.classList.add("animation-class");
-      truck?.classList.remove("centered-image-2");
-      truck?.classList.add("centered-image-3");
-      truck?.classList.remove("centered-image-4");
+  
 
-    }
-    else if(currentScroll >= 1500 && currentScroll <= 2000)
-    {
-      document.getElementById("third-animated-section")?.classList.add("animation-class");
-      truck?.classList.remove("centered-image-3");
-      truck?.classList.add("centered-image-4");
-      truck?.classList.remove("centered-image-5");
-
-    }else if(currentScroll > 2000 && currentScroll <= 2500){
-      truck?.classList.remove("centered-image-4");
-      truck?.classList.add("centered-image-5");
-      truck?.classList.remove("centered-image-stop");
-
-    }else if(currentScroll > 2500){
-      truck?.classList.remove("centered-image-5");
-      truck?.classList.add("centered-image-stop");
-    }
-
-  }
 
 }
