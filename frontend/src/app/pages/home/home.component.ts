6a64e70dc15ed1c287a422a804251be7d915f7e7
AGALMAD <agalmad@g.educaand.es>
2025-01-04 15:31:19 +0100

    cambios en header (mejor contraste e iconos)

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,17 +1,40 @@
 import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { RouterLink } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterLink],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  
+  //Lo pongo tipo map para que tenga mejor legibilidad
+  PRODUCTS = {
+    FRUIT: {
+      title: 'frutas',
+      imageUrl: '/assets/main/cesta-frutas.jpg',
+    },
+    VEGETABLE: {
+      title: 'verduras',
+      imageUrl: '/assets/main/cesta-verduras.jpg',
+    },
+    MEAT: {
+      title: 'carnes',
+      imageUrl: '/assets/main/carne-ecologica.jpg',
+    },
+  };
 
+  //Pasa los valores a un array para poder iterarlo en el html
+  productArray = Object.entries(this.PRODUCTS).map(([key, value]) => ({
+    key,
+    ...value,
+  }));
 
+  constructor(private router: Router) {}
+
+  goToRoute(route: string) {
+    this.router.navigateByUrl(route);
+  }
 }
